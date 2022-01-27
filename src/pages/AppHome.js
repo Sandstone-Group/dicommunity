0d5e1ac7ab3e824b59db77e0ff9faacc6f87b7f1
Michael Tanner <m.tanner161@gmail.com>
2022-01-26 17:17:43 -0700

    Adding dicommunity apps - still working on styling

--- src/pages/AppHome.js
+++ src/pages/AppHome.js
@@ -4,9 +4,17 @@ const AppHome = () => {
   return (
     <div className="home">
       <iframe
+        title="GIS"
+        width="1000"
+        height="600"
+        src="https://sandstone.maps.arcgis.com/apps/webappviewer/index.html?id=a8c48643ca1946549750cf38d1d2d66c"
+        frameborder="0"
+        allowFullScreen="true"
+      ></iframe>
+      <iframe
         title="DIC Demo Dashboard"
-        width="900"
-        height="500"
+        width="1000"
+        height="600"
         src="https://app.powerbi.com/view?r=eyJrIjoiYTU4MmY3MWItZmQ4Zi00NTRiLWI5ZjktZjI5YWI1MGVhYjQ4IiwidCI6IjA1MTM5NTUzLWVlOTAtNDdhZi1iNmY3LTU0ZDk2OTc4ZTQ5ZSJ9&pageName=ReportSection"
         frameborder="0"
         allowFullScreen="true"
