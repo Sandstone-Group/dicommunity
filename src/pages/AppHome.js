1c6951bd3ca88ba7da0f6f04f2d2d23db41632fb
Michael Tanner <m.tanner161@gmail.com>
2022-03-02 19:41:57 -0700

    adding some styling elements to the AppHome page

--- src/pages/AppHome.js
+++ src/pages/AppHome.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { MDBInput } from "mdbreact";
 
 const AppHome = () => {
   return (
     <>
-      <div className="home">
+      <div>
         <iframe
           title="GIS"
           width="1200"
@@ -12,14 +13,28 @@ const AppHome = () => {
           frameborder="0"
           allowFullScreen="true"
         ></iframe>
-        <iframe
-          title="DIC Demo Dashboard"
-          width="1200"
-          height="715"
-          src="https://app.powerbi.com/view?r=eyJrIjoiYTU4MmY3MWItZmQ4Zi00NTRiLWI5ZjktZjI5YWI1MGVhYjQ4IiwidCI6IjA1MTM5NTUzLWVlOTAtNDdhZi1iNmY3LTU0ZDk2OTc4ZTQ5ZSJ9&pageName=ReportSection"
-          frameborder="0"
-          allowFullScreen="true"
-        ></iframe>
+        <h2 className="home-title">Keep Track of your BLK GROUP and TRACT</h2>
+        <div className="input-group">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="basic-addon">
+              <i className="fas fa-pencil-alt prefix"></i>
+            </span>
+          </div>
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            label="Test"
+            rows="5"
+          ></textarea>
+          <iframe
+            title="DIC Demo Dashboard"
+            width="1200"
+            height="715"
+            src="https://app.powerbi.com/view?r=eyJrIjoiYTU4MmY3MWItZmQ4Zi00NTRiLWI5ZjktZjI5YWI1MGVhYjQ4IiwidCI6IjA1MTM5NTUzLWVlOTAtNDdhZi1iNmY3LTU0ZDk2OTc4ZTQ5ZSJ9&pageName=ReportSection"
+            frameborder="0"
+            allowFullScreen="true"
+          ></iframe>
+        </div>
       </div>
     </>
   );
