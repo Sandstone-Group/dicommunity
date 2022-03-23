182af8e229fdbb581d764d064ee33270e9a72001
Michael Tanner <m.tanner161@gmail.com>
2022-03-23 12:23:30 -0600

    Adding hubspot chat flow to website

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ import Community from "./pages/Community";
 import Dashboard from "./pages/Dashboard";
 import About from "./pages/About";
 import Team from "./pages/Team";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <Router>
       <Sidebar />
+      <Helmet>
+        <script
+          type="text/javascript"
+          id="hs-script-loader"
+          async
+          defer
+          src="//js-na1.hs-scripts.com/21397066.js"
+        ></script>
+      </Helmet>
       <Switch>
         <Route exact path="/dicommunity" component={HomePage} />
         <Route path="/app" exact component={AppHome} />
